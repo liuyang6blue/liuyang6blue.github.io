42ec3a7b709b02bcd0923d5ebff213e66d3dc08b
liuyang <530022025@qq.com>
2020-02-06 10:50:47 +0800

    Site updated: 2020-02-06 10:50:44

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 06 2020 10:42:10 GMT+0800 (GMT+08:00)
+// build time:Thu Feb 06 2020 10:50:39 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
