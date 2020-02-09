59d6cfa79d2388e56725ec82a75d443b9469c258
liuyang <530022025@qq.com>
2020-02-09 18:15:30 +0800

    Site updated: 2020-02-09 18:15:20

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 06 2020 10:50:39 GMT+0800 (GMT+08:00)
+// build time:Sun Feb 09 2020 18:14:57 GMT+0800 (GMT+08:00)
 var gulp=require("gulp");var minifycss=require("gulp-minify-css");var uglify=require("gulp-uglify");var htmlmin=require("gulp-htmlmin");var htmlclean=require("gulp-htmlclean");gulp.task("minify-css",function(){return gulp.src("./public/**/*.css").pipe(minifycss()).pipe(gulp.dest("./public"))});gulp.task("minify-html",function(){return gulp.src("./public/**/*.html").pipe(htmlclean()).pipe(htmlmin({removeComments:true,minifyJS:true,minifyCSS:true,minifyURLs:true})).pipe(gulp.dest("./public"))});gulp.task("minify-js",function(){return gulp.src(["./public/**/*.js","!./public/**/*.min.js"]).pipe(uglify()).pipe(gulp.dest("./public"))});gulp.task("default",gulp.parallel("minify-html","minify-css","minify-js",function(){}));
 //rebuild by neat 
