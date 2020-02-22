8f9c112a2248cc88ccdd3b03e30949232ad3e34b
Blue <530022025@qq.com>
2020-02-23 00:53:11 +0800

    Site updated: 2020-02-23 00:53:07

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -1,3 +1,3 @@
-// build time:Sun Feb 23 2020 00:09:35 GMT+0800 (GMT+08:00)
+// build time:Sun Feb 23 2020 00:53:01 GMT+0800 (GMT+08:00)
 var gulp=require("gulp");var minifycss=require("gulp-minify-css");var uglify=require("gulp-uglify");var htmlmin=require("gulp-htmlmin");var htmlclean=require("gulp-htmlclean");gulp.task("minify-css",function(){return gulp.src("./public/**/*.css").pipe(minifycss()).pipe(gulp.dest("./public"))});gulp.task("minify-html",function(){return gulp.src("./public/**/*.html").pipe(htmlclean()).pipe(htmlmin({removeComments:true,minifyJS:true,minifyCSS:true,minifyURLs:true})).pipe(gulp.dest("./public"))});gulp.task("minify-js",function(){return gulp.src(["./public/**/*.js","!./public/**/*.min.js"]).pipe(uglify()).pipe(gulp.dest("./public"))});gulp.task("default",gulp.parallel("minify-html","minify-css","minify-js",function(){}));
 //rebuild by neat 
